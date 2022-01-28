74116a8516d738f14bbf895114e2c8e23054010a
Subhrajeet Padhy <subhrajeet.padhy@gmail.com>
2022-01-28 16:59:59 +0530

    git demo1

---
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -0,0 +1,16 @@
+package com.javatechie.app.git_demo;
+
+public class App {
+
+	public void addData(String data) {
+		System.out.println("Data =" + data);
+		
+		if(data.equals(Constant.DATA_TYPE)) {
+			System.out.println("welcome");
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/main/java/com/javatechie/app/git_demo/Constant.java
@@ -0,0 +1,7 @@
+package com.javatechie.app.git_demo;
+
+public class Constant {
+
+	public static String DATA_TYPE = "Java Techie";
+	
+}
